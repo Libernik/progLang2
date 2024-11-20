0fb30f34308b6a4eaf5c37042d1e0dd336425a06
libernik <lonely_crowd@mail.ru>
2024-11-20 20:01:25 +0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'password-manager-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Установка service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Обработка запросов
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Обновление service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
